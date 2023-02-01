7ad6692a08198464a9b9191fa3cc59700575bbfc
psvelayudhan <psvelayu@gmail.com>
2023-02-01 17:56:11 -0500

    potentially breaking commit introducing QC functions

---
+++ R/quality_control.R
@@ -0,0 +1,108 @@
+#' rsfMRI QC
+#'
+#' @description
+#' Given a raw MRI QC file and a dataframe containing subjects, removes subjects
+#'  that do not pass all rsfMRI quality control and protocol compliance checks
+#'
+#' @param abcd_df A dataframe containing subjectkeys
+#' @param mriqcrp10301 The `mriqcrp10301` data object
+#' @param t The collection period of interest (defaults to baseline: 0)
+#'
+#' @return rsfmri_qc_list a list of:
+#' 1. The original dataframe with failing QC participants dropped
+#' 2. A list of subjectkeys of dropped QC participants
+#'
+#' @export
+qc_rsfmri <- function(abcd_df, mriqcrp10301, t = 0) {
+    mri_qc <- mriqcrp10301 |>
+        abcd_import(t = t, subjects = abcd_df[, "subjectkey"]) |>
+        dplyr::select("subjectkey",
+                      dplyr::contains(c("qc_score", "pc_score")),
+                      -dplyr::contains("mid"))
+    mri_qc <- Filter(function(x) !all(is.na(x)), mri_qc)
+    mri_qc <- col_to_num(mri_qc, 2:length(mri_qc))
+    mri_qc[is.na(mri_qc)] <- 1
+    rsfmri_qc <- mri_qc |>
+        dplyr::select("subjectkey", dplyr::contains("rsfmri"))
+    rsfmri_qc$min_qc <- do.call(pmin, rsfmri_qc[, 2:length(rsfmri_qc)])
+    failed_qc <- rsfmri_qc$"subjectkey"[rsfmri_qc$"min_qc" == 0]
+    print(paste0(length(failed_qc), " subjects failed rsFMRI QC"))
+    passing_qc <- which(rsfmri_qc$"min_qc" == 1)
+    abcd_df_qc <- abcd_df[passing_qc, ]
+    rsfmri_qc_list <- list(abcd_df_qc, failed_qc)
+    names(rsfmri_qc_list) <- c("qc_passing_df", "failed_qc")
+    return(rsfmri_qc_list)
+}
+
+
+#' dmri QC
+#'
+#' @description
+#' Given a raw MRI QC file and a dataframe containing subjects, removes subjects
+#'  that do not pass all dmri quality control and protocol compliance checks
+#'
+#' @param abcd_df A dataframe containing subjectkeys
+#' @param mriqcrp10301 The `mriqcrp10301` data object
+#' @param t The collection period of interest (defaults to baseline: 0)
+#'
+#' @return dmri_qc_list a list of:
+#' 1. The original dataframe with failing QC participants dropped
+#' 2. A list of subjectkeys of dropped QC participants
+#'
+#' @export
+qc_dmri <- function(abcd_df, mriqcrp10301, t = 0) {
+    mri_qc <- mriqcrp10301 |>
+        abcd_import(t = t, subjects = abcd_df[, "subjectkey"]) |>
+        dplyr::select("subjectkey",
+                      dplyr::contains(c("qc_score", "pc_score")),
+                      -dplyr::contains("mid"))
+    mri_qc <- Filter(function(x) !all(is.na(x)), mri_qc)
+    mri_qc <- col_to_num(mri_qc, 2:length(mri_qc))
+    mri_qc[is.na(mri_qc)] <- 1
+    dmri_qc <- mri_qc |>
+        dplyr::select("subjectkey", dplyr::contains("dmri"))
+    dmri_qc$min_qc <- do.call(pmin, dmri_qc[, 2:length(dmri_qc)])
+    failed_qc <- dmri_qc$"subjectkey"[dmri_qc$"min_qc" == 0]
+    print(paste0(length(failed_qc), " subjects failed dmri QC"))
+    passing_qc <- which(dmri_qc$"min_qc" == 1)
+    abcd_df_qc <- abcd_df[passing_qc, ]
+    dmri_qc_list <- list(abcd_df_qc, failed_qc)
+    names(dmri_qc_list) <- c("qc_passing_df", "failed_qc")
+    return(dmri_qc_list)
+}
+
+#' smri QC
+#'
+#' @description
+#' Given a raw MRI QC file and a dataframe containing subjects, removes subjects
+#'  that do not pass all smri quality control and protocol compliance checks
+#'
+#' @param abcd_df A dataframe containing subjectkeys
+#' @param mriqcrp10301 The `mriqcrp10301` data object
+#' @param t The collection period of interest (defaults to baseline: 0)
+#'
+#' @return smri_qc_list a list of:
+#' 1. The original dataframe with failing QC participants dropped
+#' 2. A list of subjectkeys of dropped QC participants
+#'
+#' @export
+qc_smri <- function(abcd_df, mriqcrp10301, t = 0) {
+    mri_qc <- mriqcrp10301 |>
+        abcd_import(t = t, subjects = abcd_df[, "subjectkey"]) |>
+        dplyr::select("subjectkey",
+                      dplyr::contains(c("qc_score", "pc_score")),
+                      -dplyr::contains("mid"))
+    mri_qc <- Filter(function(x) !all(is.na(x)), mri_qc)
+    mri_qc <- col_to_num(mri_qc, 2:length(mri_qc))
+    mri_qc[is.na(mri_qc)] <- 1
+    smri_qc <- mri_qc |>
+        dplyr::select("subjectkey", dplyr::contains(c("t1", "t2")))
+    smri_qc$min_qc <- do.call(pmin, smri_qc[, 2:length(smri_qc)])
+    failed_qc <- smri_qc$"subjectkey"[smri_qc$"min_qc" == 0]
+    print(paste0(length(failed_qc), " subjects failed smri QC"))
+    passing_qc <- which(smri_qc$"min_qc" == 1)
+    abcd_df_qc <- abcd_df[passing_qc, ]
+    smri_qc_list <- list(abcd_df_qc, failed_qc)
+    names(smri_qc_list) <- c("qc_passing_df", "failed_qc")
+    return(smri_qc_list)
+}
