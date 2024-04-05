30576a42bca5465f4a425143d3d745ef9223f2be
Prashanth Velayudhan <psvelayu@gmail.com>
2024-04-05 16:17:49 -0400

    minor formatting

--- R/subsetting.R
+++ R/subsetting.R
@@ -29,8 +29,6 @@ filter_timepoint <- function(abcd_df, t) {
 #'
 #' @param subject_list Dataframe containing list of required subjects.
 #'
-#' @return subject_filtered_df The dataframe filtered by subjects.
-#'
 #' @export
 filter_subjects <- function(abcd_df, subject_list = NULL) {
     if (is.null(subject_list)) {
@@ -44,22 +42,20 @@ filter_subjects <- function(abcd_df, subject_list = NULL) {
 
 #' Filter dataframe by time and subjectkey, then sort by subjectkey
 #'
-#' @param abcd_df A raw ABCD dataframe
+#' @param abcd_df A raw ABCD dataframe.
 #'
-#' @param t Integer representing which follow-up year to filter to. Defaults
-#' to 0 (baseline).
+#' @param t Integer representing follow-up year to filter to. Defaults to 0
+#' (baseline).
 #'
-#' @param subjects Dataframe containing list of required subjects
-#'
-#' @return abcd_clean_df The subsetted dataframe
+#' @param subject_list List of subjects to filter to.
 #'
 #' @export
 time_subject_filter_sort <- function(abcd_df, t = NULL, subject_list = NULL) {
-    abcd_clean_df <- abcd_df |>
+    abcd_filtered_df <- abcd_df |>
         filter_timepoint(t) |>
         filter_subjects(subject_list) |>
         numcol_to_numeric()
-    abcd_clean_df <- abcd_clean_df |>
-        dplyr::arrange(abcd_clean_df$"subjectkey")
-    return(abcd_clean_df)
+    abcd_filtered_and_sorted_df <- abcd_filtered_df |>
+        dplyr::arrange(abcd_filtered_df$"subjectkey")
+    return(abcd_filtered_and_sorted_df)
 }
