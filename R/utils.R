3cb106858473d0412bc868bdfa8d789d778e1f2f
Prashanth Velayudhan <psvelayu@gmail.com>
2024-04-05 16:10:30 -0400

    same as last commit

--- R/utils.R
+++ R/utils.R
@@ -1,21 +1,3 @@
-#' Dummify categorical columns
-#'
-#' Wrapper function for fsatDummies::dummy_cols
-#'
-#' @param abcd_df Dataframe containing column to be dummied
-#' @param cols Columns to be dummied
-#'
-#' @return abcd_df Dataframe in dummy format
-#'
-#' @export
-dummy <- function(abcd_df, cols) {
-    abcd_df <- fastDummies::dummy_cols(
-        .data = abcd_df,
-        select_columns = cols,
-        remove_selected_columns = TRUE)
-    return(abcd_df)
-}
-
 #' Convert specific columns to numeric
 #'
 #' @param df The dataframe containing columns to be converted
